55b1e5413ba6e8c8007672f3b1f3df9dfc982bf3
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2024-11-16 13:47:23 -0300

    MyOFT contract: LayerZero OFT contract

---
+++ packages/hardhat/contracts/mocks/MyOFT.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+
+/// @notice OFT is an ERC-20 token that extends the OFTCore contract.
+contract MyOFT is OFT {
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    address _lzEndpoint,
+    address _delegate
+  ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {}
+
+  function mint(address recipient, uint256 amount) external onlyOwner {
+    return _mint(recipient, amount);
+  }
+}
