78b9f4646bd8a53d9b7808fcafc4e62264cb9dd1
selimerunkut <se@cypherx.tech>
2024-11-19 22:42:22 +0700

    correct the pepo token address for Taiko

--- packages/nextjs/utils/token/index.ts
+++ packages/nextjs/utils/token/index.ts
@@ -3,5 +3,5 @@ export * from "./requestApproval";
 // baseSepolia
 //export const TokenAddress = '0x97C5Cc720e8b170AAaF00c6722230ae60d2Aa88f'
 // taikoHekla
-export const TokenAddress = '0x97C5Cc720e8b170AAaF00c6722230ae60d2Aa88f'
+export const TokenAddress = '0x25A31827bc8F02222a578e8EE8f4c862bEd11F64'
 export const PayTokenAddress = '0x036CbD53842c5426634e7929541eC2318f3dCF7e'
