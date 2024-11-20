8a4f4919d400b3a48990bffe6407c9c90d5ed520
naftalimurgor <murgornaftali@gmail.com>
2024-11-20 20:33:21 +0300

    wrap fetchPriceFromBang & hasApproval async call in try-catch block #3

--- packages/nextjs/utils/bang/fetchPriceFromBang.ts
+++ packages/nextjs/utils/bang/fetchPriceFromBang.ts
@@ -1,22 +1,26 @@
-import {ethers} from "ethers";
-import {BangContractAddress} from "~~/utils/bang/index";
-import {TokenAddress} from "~~/utils/token";
+import { ethers } from "ethers";
+import { BangContractAddress } from "~~/utils/bang/index";
+import { TokenAddress } from "~~/utils/token";
 
 export async function fetchPriceFromBang(fromAmount: number, provider: any): Promise<number> {
-    if(fromAmount === 0) return 0;
-    // Replace with the deployed contract address
-    const contractAddress = BangContractAddress;
+    try {
+        if (fromAmount === 0) return 0;
+        // Replace with the deployed contract address
+        const contractAddress = BangContractAddress;
 
-    // ABI of the contract
-    const contractABI = [
-        "function computeAmountOut(address tokenIn, uint256 amountIn) public view returns (uint256)"
-    ];
+        // ABI of the contract
+        const contractABI = [
+            "function computeAmountOut(address tokenIn, uint256 amountIn) public view returns (uint256)"
+        ];
 
-    // Create a contract instance
-    const contract = new ethers.Contract(contractAddress, contractABI, provider);
+        // Create a contract instance
+        const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
-    // Define your token address and amount
-    const amountIn = ethers.parseUnits(fromAmount.toString(), 18); // Replace with the amount you want to test
-    const amountOut = await contract.computeAmountOut(TokenAddress, amountIn);
-    return ethers.formatUnits(amountOut, 6) / ethers.formatEther(amountIn);
+        // Define your token address and amount
+        const amountIn = ethers.parseUnits(fromAmount.toString(), 18); // Replace with the amount you want to test
+        const amountOut = await contract.computeAmountOut(TokenAddress, amountIn);
+        return ethers.formatUnits(amountOut, 6) / ethers.formatEther(amountIn);
+    } catch (error) {
+        console.log(error)
+    }
 }
